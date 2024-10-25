2187eb11409612d6c9bce8380ecd1a8e57948232
Jose Mendez <jjmendezrodriguez@yahoo.com>
2024-10-25 13:06:45 -0400

    update file

--- javaScript/level_2/conditionals.js
+++ javaScript/level_2/conditionals.js
@@ -4,10 +4,10 @@
 //   logica = lo que escribes dentro de (parentesis)
 if (sum < 21) {
   // si tu logica se cumple: True = ejecuta lo que esta dentro de las {llaves}
-  console.log("Do you want to draw a new card?");
+  console.log("Do you want to draw a new card? 🙂");
   // si es False, puedes tener un else if es otra logica en caso de la primera es False
 } else if (sum === 21) {
-  console.log();
+  console.log("Wohoo! You've got Blackjack! 🥳");
 } else if (sum > 21) {
   console.log("You're out of the game! 😭");
 }
