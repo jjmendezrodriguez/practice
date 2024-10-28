219fda41a52d2709be41837c04b8463cfe266c84
Jose Mendez <jjmendezrodriguez@yahoo.com>
2024-10-28 01:02:25 -0400

    move to jmschoolcode

--- javaScript/README.md
+++
@@ -1,3 +0,0 @@
-# We are going to learnd JavaScript
-
-**Start your journy with the basic, then move to the next level and become a pro!! a programer!! in javaScript.😁**
--- javaScript/basic/errors.js
+++
@@ -1,7 +0,0 @@
-/*
-Los syntax errors o errores de sintaxis son:
-Errores en el código que ocurren cuando el intérprete
-o compilador encuentra algo mal escrito o que no sigue
-las reglas del lenguaje de programación. Estos errores
-previenen que el programa se ejecute hasta que sean corregidos. 
-*/
